c5818d72fee8c63c27ec65119f5e2bf1c509c828
Kathryn <kdboudreau1@gmail.com>
2019-04-15 18:45:16 -0400

    adding pictures and tailoring heading and portfolio app components

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('restaurantapp-angular App', () => {
+describe('portfolioapp-angular App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
