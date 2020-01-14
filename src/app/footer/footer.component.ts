fcbc2da9ced7a2ced418810b92af48e52b8f697c
Kathryn <kdboudreau1@gmail.com>
2020-01-14 13:51:22 -0500

    fixing tableau icon and margins in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-
-// library.add(fab);
-
 
 @Component({
   selector: 'app-footer',
