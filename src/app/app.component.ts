0de1751efdf64687b38d97fc40d734a7b7fff61f
Kathryn <kdboudreau1@gmail.com>
2020-01-14 12:18:48 -0500

    updating to match me project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
+
+// library.add(fab);
 
 @Component({
   selector: 'app-root',
