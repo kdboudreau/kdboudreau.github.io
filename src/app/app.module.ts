e41daf4ac91a373931e3e158fdd0adddb6b33466
Kathryn <kdboudreau1@gmail.com>
2020-01-14 15:25:26 -0500

    updating me branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
